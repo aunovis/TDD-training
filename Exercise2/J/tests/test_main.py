cf2e5701931428afeeda1039cd8a544fa17be4fc
Jörg Bauder <Bauder@aunovis.de>
2020-01-30 14:11:09 +0100

    testcase

--- Exercise2/J/tests/test_main.py
+++ Exercise2/J/tests/test_main.py
@@ -1,13 +1,18 @@
 import unittest
 from package import Main
 
-class TestMain(unittest.TestCase):
-    def test(self):
-        self.assertEqual(1, 1)
 
-    def testFunction(self):
-        result = Main.test(1)
-        self.assertEqual(result, 1)
-		
+class TestMain(unittest.TestCase):
+    def testIf(self):
+        Main.initMap(5)
+        Main.initRobot((0,0),'S')
+        command1 = ["f","b","f","l","f","f","f","f","r","f","f","f","f","b","r","b","l","l","f","f","r","f","f"]
+        self.assertEqual(Main.drive(), "success")
+        self.assertEqual(Main.getRoverPosition(), (2,2))
+        self.assertEqual(Main.getRoverOrientation(), "N")
+        Main.addObstacle((2,1))
+        self.assertEqual(Main.drive("f"), "Obstacle at 2,1")
+
+
 if __name__ == '__main__':
     unittest.main()
