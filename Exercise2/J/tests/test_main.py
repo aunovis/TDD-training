1c2645dd4974d81663879da2b18b26ba963cea42
Jörg Bauder <Bauder@aunovis.de>
2020-01-30 14:13:01 +0100

    ficed test mistake

--- Exercise2/J/tests/test_main.py
+++ Exercise2/J/tests/test_main.py
@@ -7,7 +7,7 @@ class TestMain(unittest.TestCase):
         Main.initMap(5)
         Main.initRobot((0,0),'S')
         command1 = ["f","b","f","l","f","f","f","f","r","f","f","f","f","b","r","b","l","l","f","f","r","f","f"]
-        self.assertEqual(Main.drive(), "success")
+        self.assertEqual(Main.drive(command1), "success")
         self.assertEqual(Main.getRoverPosition(), (2,2))
         self.assertEqual(Main.getRoverOrientation(), "N")
         Main.addObstacle((2,1))
