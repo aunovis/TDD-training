570a05fc0cb1f69cb3fd36dd559daa3e48fd09e2
Raffael Thome <thome@aunovis.de>
2020-01-31 09:07:07 +0100

    Add tests for rover initialization

--- Exercise2/DanielRaffael/Hangout2-test/rover.cpp
+++ Exercise2/DanielRaffael/Hangout2-test/rover.cpp
@@ -6,5 +6,23 @@ using namespace testing;
 
 namespace Hangout2::tests
 {
+	TEST(RoverTests, DefaultRoverState)
+	{
+		Grid grid{};
+		Rover rover{ grid, {0, 0}, Dir::N };
 
+		ASSERT_THAT(rover.Direction(), Eq(Dir::N));
+		ASSERT_THAT(rover.Position().x, Eq(0));
+		ASSERT_THAT(rover.Position().y, Eq(0));
+	}
+
+	TEST(RoverTests, InitializePositionAndDirection)
+	{
+		Grid grid{};
+		Rover rover{ grid, {99, 512}, Dir::E };
+
+		ASSERT_THAT(rover.Direction(), Eq(Dir::E));
+		ASSERT_THAT(rover.Position().x, Eq(99));
+		ASSERT_THAT(rover.Position().y, Eq(512));
+	}
 }
