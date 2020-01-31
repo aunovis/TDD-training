de7f6bc8894e17d7f5c72d38f62b8f944fb4fbf7
Raffael Thome <thome@aunovis.de>
2020-01-31 09:00:45 +0100

    Add basic types and structures

--- Exercise2/DanielRaffael/libHangout2/rover.h
+++ Exercise2/DanielRaffael/libHangout2/rover.h
@@ -2,5 +2,33 @@
 
 namespace Hangout2
 {
+	struct Pos
+	{
+		int x, y;
+	};
 
+	enum class Dir
+	{
+		N,
+		W,
+		S,
+		E,
+	};
+
+	enum class TickResult
+	{
+		Idle,
+		Running,
+		ObstacleEncountered,
+	};
+
+	class Grid
+	{
+
+	};
+
+	class Rover
+	{
+
+	};
 }
