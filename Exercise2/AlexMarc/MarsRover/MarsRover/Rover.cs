6086e0c501eb7a1123d00850edf1c0ce7a9b45c0
Marc Ketzel <ketzel@aunovis.de>
2020-02-06 14:52:56 +0100

    Backward driving and turning

--- Exercise2/AlexMarc/MarsRover/MarsRover/Rover.cs
+++ Exercise2/AlexMarc/MarsRover/MarsRover/Rover.cs
@@ -5,7 +5,7 @@
         private (int x, int y) _position;
         public (int x, int y) Position => _position;
 
-        private Direction Direction { get; }
+        public Direction Direction { get; private set; }
 
         public Rover(int x, int y, Direction direction)
             : this((x, y), direction)
@@ -21,20 +21,33 @@
         {
             foreach (var command in commands)
             {
-                var move = command == 'f' ? 1 : (command == 'r' ? -1 : 0);
-                switch (Direction)
+                switch (command)
                 {
-                    case Direction.North:
-                        _position.y -= move;
-                        break;
-                    case Direction.South:
-                        _position.y += move;
-                        break;
-                    case Direction.West:
-                        _position.x -= move;
-                        break;
-                    case Direction.East:
-                        _position.x += move;
+                    case 'f':
+                    case 'b':
+                        {
+                            var move = command == 'f' ? 1 : -1;
+                            switch (Direction)
+                            {
+                                case Direction.North:
+                                    _position.y -= move;
+                                    break;
+                                case Direction.South:
+                                    _position.y += move;
+                                    break;
+                                case Direction.West:
+                                    _position.x -= move;
+                                    break;
+                                case Direction.East:
+                                    _position.x += move;
+                                    break;
+                            }
+
+                            break;
+                        }
+                    case 'r':
+                    case 'l':
+                        Direction = (Direction)(((int)Direction + (command == 'r' ? 1 : -1) + 4) % 4);
                         break;
                 }
             }
--- Exercise2/AlexMarc/MarsRover/MarsRoverTests/MarsRoverTests.cs
+++ Exercise2/AlexMarc/MarsRover/MarsRoverTests/MarsRoverTests.cs
@@ -41,5 +41,70 @@ namespace MarsRoverTests
             Assert.Equal((4, 4), rover.Position);
         }
 
+        [Fact]
+        public void ExecutesBackwardNorthCommand()
+        {
+            var rover = new Rover(3, 4, Direction.North);
+            var command = new[] { 'b' };
+            rover.Execute(command);
+            Assert.Equal((3, 5), rover.Position);
+        }
+
+        [Fact]
+        public void ExecutesBackwardWestCommand()
+        {
+            var rover = new Rover(3, 4, Direction.West);
+            var command = new[] { 'b' };
+            rover.Execute(command);
+            Assert.Equal((4, 4), rover.Position);
+        }
+
+        [Fact]
+        public void ExecutesBackwardSouthCommand()
+        {
+            var rover = new Rover(3, 4, Direction.South);
+            var command = new[] { 'b' };
+            rover.Execute(command);
+            Assert.Equal((3, 3), rover.Position);
+        }
+
+        [Fact]
+        public void ExecutesBackwardEastCommand()
+        {
+            var rover = new Rover(3, 4, Direction.East);
+            var command = new[] { 'b' };
+            rover.Execute(command);
+            Assert.Equal((2, 4), rover.Position);
+        }
+
+        [Fact]
+        public void ExecutesTurnLeftCommand()
+        {
+            var rover = new Rover(3, 4, Direction.North);
+            var command = new[] { 'l' };
+            rover.Execute(command);
+            Assert.Equal(Direction.West, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.South, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.East, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.North, rover.Direction);
+        }
+
+        [Fact]
+        public void ExecutesTurnRightCommand()
+        {
+            var rover = new Rover(3, 4, Direction.North);
+            var command = new[] { 'r' };
+            rover.Execute(command);
+            Assert.Equal(Direction.East, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.South, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.West, rover.Direction);
+            rover.Execute(command);
+            Assert.Equal(Direction.North, rover.Direction);
+        }
     }
 }
