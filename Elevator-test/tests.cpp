6658f8f03e6a2a379d20bcdd0308d1c00da0321d
Raffael Thome <mail@rmt.name>
2020-03-05 15:37:20 +0100

    Add some declarations and a test for lift initialization

--- Elevator-test/tests.cpp
+++ Elevator-test/tests.cpp
@@ -1,8 +1,21 @@
 #include "gmock/gmock.h"
 
+#include "elevator.h"
+
 using namespace testing;
 
 namespace elevator::tests
 {
+	TEST(ElevatorTests, LiftInstallation)
+	{
+		const size_t expected_floor_count = 7;
 
+		LiftInstallation installation;
+		Lift lift;
+		std::tie(installation, lift) = lift_create_installation(expected_floor_count);
+
+		ASSERT_THAT(installation.floor_count, Eq(expected_floor_count));
+		ASSERT_THAT(lift.current_floor, Eq(0));
+		ASSERT_THAT(lift.destination_floor, Eq(0));
+	}
 }
--- libElevator/elevator.h
+++ libElevator/elevator.h
@@ -1,6 +1,53 @@
 #pragma once
 
+#include <cstdint>
+#include <functional>
+#include <tuple>
+
 namespace elevator
 {
+	using FloorIndex = int32_t;
 
+	enum class Direction
+	{
+		Up,
+		Down,
+	};
+
+	struct LiftSignals
+	{
+		std::function<void(FloorIndex)> at_floor;
+		std::function<void()> doors_opened;
+		std::function<void()> doors_closed;
+	};
+
+	struct Lift
+	{
+		FloorIndex current_floor;		// Floor that the lift is current on/moving past
+		FloorIndex destination_floor;	// Destination floor of the currently served request
+
+		LiftSignals signals;			// Signals to observe what the lift is doing
+	};
+
+	struct Floor
+	{
+		bool has_request;
+		Direction direction;
+	};
+
+	struct LiftInstallation
+	{
+		size_t floor_count;
+		Floor floors[];
+	};
+
+	std::tuple<LiftInstallation, Lift> lift_create_installation(size_t floor_count)
+	{
+		return std::make_tuple(LiftInstallation{}, Lift{});
+	}
+
+	void lift_tick(LiftInstallation const& installation, Lift const& lift)
+	{
+
+	}
 }
