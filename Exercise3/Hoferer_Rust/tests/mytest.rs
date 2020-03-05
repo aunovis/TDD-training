30e325b921fdc17a4c57c17cb1aeae368bf44b8a
Michael Hoferer <hoferer@aunovis.de>
2020-03-05 14:59:14 +0100

    Deleted Rust experiment

--- Exercise3/Hoferer_Rust/tests/mytest.rs
+++
@@ -1,9 +0,0 @@
-use adlib::*;
-
-#[test]
-fn first_test() {
-    let mut lifty = reset_lift();
-    assert_eq!(get_lift_position(lifty), 0);
-    lifty = call_lift_to_pos(&lifty, 2);
-    assert_eq!(get_lift_position(lifty), 2);
-}
