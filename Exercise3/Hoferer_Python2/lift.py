9104c94c77186ad963249258a4e39066dbe5e54c
Michael Hoferer <hoferer@aunovis.de>
2020-03-11 15:57:48 +0100

    Ex3: added ding, door handling and samefloor situation

--- Exercise3/Hoferer_Python2/lift.py
+++ Exercise3/Hoferer_Python2/lift.py
@@ -1,3 +1,15 @@
+def close_the_doors():
+    print("...-> closing doors <-...")
+
+
+def open_the_doors():
+    print("...<- opening doors ->...")
+
+
+def ding():
+    print("DING!")
+
+
 class Lift:
     def __init__(self, current_floor=0):
         self.current_floor = current_floor
@@ -6,21 +18,30 @@ class Lift:
 
     def order(self, order):
         print("Getting your request for a lift:", order[0], order[1].name)
-        self.move_to_floor(order[0])
+        close_the_doors()
+        self.move_to_floor(0, order[0])
 
-    def move_to_floor(self, target_floor):
+    def move_to_floor(self, tracker, target_floor):
         if target_floor > self.current_floor:
             self.current_floor = self.current_floor + 1
             print("Moving up, passing floor", self.current_floor)
-            return self.move_to_floor(target_floor)
+            tracker = tracker + 1
+            return self.move_to_floor(tracker, target_floor)
         elif target_floor < self.current_floor:
             self.current_floor = self.current_floor - 1
             print("Moving down, Passing floor", self.current_floor)
-            return self.move_to_floor(target_floor)
+            tracker = tracker + 1
+            return self.move_to_floor(tracker, target_floor)
         else:
-            print("Arrived at floor", target_floor, "... opening doors")
+            if tracker == 0:
+                print("Already here! Leaving doors open ...", target_floor)
+            else:
+                print("Arrived at floor", target_floor)
+                ding()
+                open_the_doors()
             return
 
     def choose_target_floor(self, floor):
         print("Getting your request for target floor:", floor)
-        self.move_to_floor(floor)
+        close_the_doors
+        self.move_to_floor(0, floor)
