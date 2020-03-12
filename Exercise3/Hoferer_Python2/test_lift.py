b53df3d23f40f0e1ec2c6b2aa35e526ca8023254
Michael Hoferer <hoferer@aunovis.de>
2020-03-12 17:05:55 +0100

    Ex3: uncommented data for parametrized test execution

--- Exercise3/Hoferer_Python2/test_lift.py
+++ Exercise3/Hoferer_Python2/test_lift.py
@@ -7,7 +7,7 @@ from direction import *
 @pytest.mark.parametrize(
     'start_pos, output', [
         (1, 1),
-        # (15, 15)
+        (15, 15)
     ]
 )
 def test_lift_init_with_floor(start_pos, output):
@@ -22,9 +22,9 @@ def test_lift_init():
 
 @pytest.mark.parametrize(
     'start_pos, order, output', [
-        # (0, (5, Direction.UP), 5),
-        # (-1, (2, Direction.DOWN), 2),
-        # (5, (2, Direction.UP,), 2),
+        (0, (5, Direction.UP), 5),
+        (-1, (2, Direction.DOWN), 2),
+        (5, (2, Direction.UP,), 2),
         (8, (1, Direction.DOWN), 1)
     ]
 )
@@ -36,11 +36,11 @@ def test_order_lift(start_pos, order, output):
 
 @pytest.mark.parametrize(
     'start_pos, order, target_pos, output', [
-        # (-5, (5, Direction.UP), 8, 8),
-        # (10, (2, Direction.DOWN), -1, -1),
-        # (0, (2, Direction.UP), 4, 4),
-        # (5, (1, Direction.DOWN), 5, 5),
-        # (2, (4, Direction.UP), 4, 4),
+        (-5, (5, Direction.UP), 8, 8),
+        (10, (2, Direction.DOWN), -1, -1),
+        (0, (2, Direction.UP), 4, 4),
+        (5, (1, Direction.DOWN), 5, 5),
+        (2, (4, Direction.UP), 4, 4),
         (10, (15, Direction.UP), 12, 12)
     ]
 )
